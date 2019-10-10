232a0d0997c9e53ac3ecf8087bfea98c1e7d555b
Linhárt Ervin <LinhartE@olajterv.hu>
2019-10-10 09:28:44 +0200

    simple javascript code

---
+++ js/sample.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Order placed, check your email!";
+    document.getElementById("button").style.display = "none";
+}
